d00d56190faccd56a1104eda0c51dfe1b453ecfb
Cristina Coutinho <cris.coutinho02@gmail.com>
2021-12-20 16:09:48 -0300

    ajustando repo

---
+++ area.go
@@ -0,0 +1,19 @@
+package area
+
+//Pi é uma proporçao numerica definifa pela relação entre o perímetro de uma circunferência
+//e seu diâmetro
+const Pi = 3.1416
+
+//Circ é responsavel por calcular a area da circunferencia
+func Circ(raio float64) float64 {
+	return math.Pow(raio, 2) * Pi
+}
+
+//Rect é responsavel por calcular a area de um retângulo
+func Rect(base, altura) float64 {
+	return base * altura
+}
+
+func _TrianguloEq(base, altura float64) float64 {
+	return (base * altura) / 2
+}
